e38d2e3d9d3824d30803da0a6dcdb8c1607c3801
Gonzalo Butafuoco <butafuocogonzalo@gmail.com>
2023-08-08 18:16:43 -0300

    acomodamiento del fondo
    se acomodo el fondo y se realizo una estilización leve

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 export const Home = () => {
   return (
     <div>
-    <body className='w-screen h-screen bg-no-repeat bg-cover ' style={{ backgroundImage: 'url("https://www.parati.com.ar/wp-content/uploads/2022/02/casa-prefabricada-de-hormipresa-en-el-boecillo-valladolid-realizada-con-el-sistema-arctic-wall-de-paneles-estructurales-con-el-acabado-incorporado_6f2a28cd_1280x794.jpeg")' }}>
-      <h1>Home Page</h1>
+    <body className='w-full h-screen bg-no-repeat bg-cover bg-left bg-fixed bg-[url("https://i.imgur.com/zXxczpB.jpg")]'>
+      <h1 className=' absolute p-10'>Home Page</h1>
     </body>
       
   </div>
