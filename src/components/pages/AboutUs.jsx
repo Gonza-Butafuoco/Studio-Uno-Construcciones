8bd83b932aad8fb0aadce9a97986a30bbced880a
CagianesaEmiliano <129976905+CagianesaEmiliano@users.noreply.github.com>
2023-08-08 21:14:46 -0300

    Cambios en contactos y abouteus
    Le puse position absolute y top en 0 al body de contact y aboutus  para que no salga la barra de scroll como al home.

--- src/components/pages/AboutUs.jsx
+++ src/components/pages/AboutUs.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 export const AboutUs = () => {
   return (
     <div>
-    <body className=' bg-cyan-500 h-screen'>
-      <h1>About us Page</h1>
+    <body className='absolute top-0 bg-cyan-500 h-screen'>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <h1 className='z-10 '>About us Page</h1>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente facere temporibus recusandae nisi asperiores, aliquam quisquam eius reprehenderit, ad magnam accusamus cupiditate, labore dolorem aspernatur eum! Tempora natus aliquam odit!</p>
     </body>
       
--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 export const Contact = () => {
   return (
     <div>
-    <body className=' bg-cyan-500 h-screen'>
+    <body className='absolute top-0 bg-cyan-500 h-screen'>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
       <h1>Contact Page</h1>
 
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente facere temporibus recusandae nisi asperiores, aliquam quisquam eius reprehenderit, ad magnam accusamus cupiditate, labore dolorem aspernatur eum! Tempora natus aliquam odit!</p>
