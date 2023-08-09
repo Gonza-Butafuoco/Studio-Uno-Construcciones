d3c1858f19cbb63c5ac00ddaf5d337c6a4d7aedf
Gonzalo Butafuoco <butafuocogonzalo@gmail.com>
2023-08-08 21:12:53 -0300

    Estilización del home fuente y color
    Se estilizo la fuente y el color de la misma en el home aparte de mover el contenido de una forma mas adecuada

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: { 
+      fontFamily: {
+      'space-grotesk': ['Space Grotesk'],
+    },
+  },
   },
   plugins: [],
 }
