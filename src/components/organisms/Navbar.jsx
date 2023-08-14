052c3b1de43535aae0f7431c2bb4eab5fb8c4ebd
Gonzalo Butafuoco <butafuocogonzalo@gmail.com>
2023-08-14 17:16:31 -0300

    Responsive desing Home y Navbar
    Se hizo la parte responsive del home y sus estilos. Tambien se modifico la barra de navegación

--- src/components/organisms/Navbar.jsx
+++ src/components/organisms/Navbar.jsx
@@ -1,24 +1,68 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import ButtonSection from '../molecules/ButtonSection'
-import svgimage from "../Resources/studiouno_logotipo.svg"
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import ButtonSection from '../molecules/ButtonSection';
+import svgimage from "../Resources/studiouno_logotipo.svg";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className='sticky top-0 bg-white z-10 '>
-        <nav className=' flex  border  border-black'>
-            <div>
-              <Link to="/">
-                <img   src={svgimage} alt="logo" className=' p-2 m-2 h-20 w-20' />
-              </Link>
+    <div className='sticky top-0 bg-white z-10'>
+      <nav className='flex justify-between items-center border border-black p-4 md:p-6'>
+        <div>
+          <Link to="/">
+            <img src={svgimage} alt="logo" className='h-16 w-16' />
+          </Link>
+        </div>
+        <div className='md:hidden'>
+          <button onClick={toggleMenu} className='p-2'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+              className='h-6 w-6'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M4 6h16M4 12h16M4 18h16'
+              />
+            </svg>
+          </button>
+          {isMenuOpen && (
+            <div className='absolute top-16 right-4 bg-white border border-black p-2'>
+              <ul className='space-y-2 text-sm'>
+                <li>
+                  <Link to='/'>Home</Link>
+                </li>
+                <li>
+                  <Link to='/AboutUs'>About Us</Link>
+                </li>
+                <li>
+                  <Link to='/ModelsPage'>Models</Link>
+                </li>
+                <li>
+                  <Link to='/Contact'>Contact</Link>
+                </li>
+              </ul>
             </div>
-            <div className=' flex  mx-auto '>
-                <ButtonSection/>
-            </div>
-        </nav>
+          )}
+        </div>
+        <div className='hidden md:flex'>
+          <ButtonSection />
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
+
+
 
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,21 +2,19 @@ import React from "react";
 
 export const Home = () => {
   return (
-    <div>
-      <body className='absolute top-0 w-full h-screen bg-no-repeat bg-cover bg-left bg-fixed bg-[url("https://i.imgur.com/zXxczpB.jpg")]'>
-        <div>
-          <h1 className=" font-space-grotesk absolute z-1 left-28 top-60 text-9xl bg-gradient-to-r text-transparent from-green-400 via-blue-500 to-blue-900 bg-clip-text">
-            ¡Bienvenidos!
-          </h1>
-          <p className=" font-space-grotesk absolute left-28 top-[450px] text-5xl bg-gradient-to-r text-transparent from-green-400 via-blue-500 to-blue-900 bg-clip-text ">
-            Explora la fusión perfecta entre comodidad<br/> y estilo en nuestras
-            casas premoldeadas.<br/> 
-            Descubre un mundo de diseño innovador y<br/>
-            construcción eficiente.<br/>
-            Bienvenido a tu hogar ideal.
-          </p>
-        </div>
-      </body>
+    <div className='w-full h-screen bg-[url("https://i.imgur.com/zXxczpB.jpg")] bg-cover bg-fixed overflow-hidden'>
+      <div className="flex flex-col items-center justify-center h-full text-white px-4 md:px-0">
+        <h1 className="font-space-grotesk text-5xl md:text-9xl bg-gradient-to-r text-transparent from-green-400 via-blue-500 to-blue-900 bg-clip-text mb-8 text-center">
+          ¡Bienvenidos!
+        </h1>
+        <p className="font-space-grotesk text-xl md:text-5xl text-center bg-gradient-to-r text-transparent from-green-400 via-blue-500 to-blue-900 bg-clip-text">
+          Explora la fusión perfecta entre comodidad<br/> y estilo en nuestras
+          casas premoldeadas.<br/> 
+          Descubre un mundo de diseño innovador y<br/>
+          construcción eficiente.<br/>
+          Bienvenido a tu hogar ideal.
+        </p>
+      </div>
     </div>
   );
 };
