c2df6d2e863a45fc6e2d63a8f887b560ca0bdcfd
Gonzalo Butafuoco <butafuocogonzalo@gmail.com>
2023-09-01 20:03:49 -0300

    Reestilización de la pagina y arreglos de contenido
    Se cambio el estilo de la pagina al pedido del cliente y se modifico el contenido de los planos para solamente dejar las imagenes mas nitidas. Aparte se modifico el favicon y se agrego para distintos dispositivos aparte de cambiar el nombre por el del dominio

--- src/components/pages/AboutUs.jsx
+++ src/components/pages/AboutUs.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export const AboutUs = () => {
   return (
-    <div className='w-full h-screen bg-[url("https://i.imgur.com/O1kPyxm.png")] bg-cover bg-fixed overflow-hidden'>
+    <div className='w-full h-screen bg-sky-200 bg-cover bg-fixed overflow-hidden'>
       <div className="flex flex-col items-center justify-center h-full text-white px-4 md:px-0">
-        <h1 className="font-space-grotesk text-5xl md:text-9xl bg-gradient-to-r text-transparent from-green-400 via-blue-500 to-blue-900 bg-clip-text mb-8 text-center">
+        <h1 className="font-space-grotesk text-5xl md:text-9xl bg-gradient-to-r text-transparent from-blue-600 via-cyan-600-600 to-sky-600 bg-clip-text mb-8 text-center">
           Sobre nosotros
         </h1>
-        <p className="font-space-grotesk text-xl  md:text-3xl md:mx-40 text-center bg-gradient-to-r text-transparent from-green-400 via-blue-500 to-blue-900 bg-clip-text ">
+        <p className="font-space-grotesk text-xl  md:text-3xl md:mx-40 text-center bg-gradient-to-r text-transparent from-blue-600 via-cyan-600-600 to-sky-600 bg-clip-text ">
           Studio Uno Construcciones es una destacada empresa en el sector de la
           construcción de casas prefabricadas. Con años de experiencia en el
           mercado, nos enorgullece ofrecer soluciones habitacionales de alta
